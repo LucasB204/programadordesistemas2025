145aec194e14edc7ce5a919ebf081853ae3e76cd
LucasB204 <l.b.sgamers@gmail.com>
2025-06-23 17:30:11 -0300

    jogo da velha

---
+++ Jogo da Velha/script.js
@@ -0,0 +1,21 @@
+var jogador= "x";
+
+function jogar(celula){
+
+    //compara se a célula está vazia para assim escrever
+    if(celula.innerHTML == ""){
+        
+        //escreva a letra na célula
+        celula.innerHTML=jogador;
+
+        //alterna a variável jogador para a proxima jogada
+        if(jogador == "x"){
+            jogador = "o";
+            celula.style.backgroundColor= "red";
+
+        }else{
+            jogador = "x";
+            celula.style.backgroundColor= "blue";
+        }
+    }
+}
